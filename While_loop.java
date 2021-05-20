962e9759868bf777e22dec90a1289056a59019e1
Jayendra <jayendra05mishra@gmail.com>
2021-05-20 15:45:54 +0530

    change made

--- While_loop.java
+++ While_loop.java
@@ -2,7 +2,7 @@ public class While_loop {
 
     public static void main(String[] args) {
         int i=1;
-        while(i<=10){
+        while(i<=15){
             System.out.println(i);
             i++;
         }
